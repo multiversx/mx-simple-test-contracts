57da9996ed4766c240f670cde79e692b6a4836f6
Alin Cruceat <alyn_509@yahoo.com>
2024-07-30 15:54:20 +0300

    v1

--- simple-esdt-test/src/simple_esdt_test.rs
+++ simple-esdt-test/src/simple_esdt_test.rs
@@ -3,7 +3,6 @@
 #[allow(unused_imports)]
 use multiversx_sc::imports::*;
 
-/// An empty contract. To be used as a template when starting a new contract from scratch.
 #[multiversx_sc::contract]
 pub trait SimpleEsdtTest {
     #[init]
@@ -11,4 +10,39 @@ pub trait SimpleEsdtTest {
 
     #[upgrade]
     fn upgrade(&self) {}
+
+    #[endpoint(ESDTLocalMint)]
+    fn esdt_local_mint(&self, token_id: TokenIdentifier, nonce: u64, amount: BigUint) {
+        self.send().esdt_local_mint(&token_id, nonce, &amount);
+    }
+
+    #[endpoint(ESDTLocalBurn)]
+    fn esdt_local_burn(&self, token_id: TokenIdentifier, nonce: u64, amount: BigUint) {
+        self.send().esdt_local_burn(&token_id, nonce, &amount);
+    }
+
+    #[endpoint(ESDTNFTCreate)]
+    fn esdt_nft_create(
+        &self,
+        token_id: &TokenIdentifier,
+        amount: &BigUint,
+        name: &ManagedBuffer,
+        royalties: &BigUint,
+        hash: &ManagedBuffer,
+        attributes: &ManagedBuffer,
+        uris: &ManagedVec<ManagedBuffer>,
+    ) {
+        self.send()
+            .esdt_nft_create(&token_id, &amount, name, royalties, hash, attributes, uris);
+    }
+
+    #[endpoint(ESDTNFTAddQuantity)]
+    fn esdt_nft_add_quantity(&self, token_id: TokenIdentifier, nonce: u64, amount: BigUint) {
+        self.send().esdt_local_mint(&token_id, nonce, &amount);
+    }
+
+    #[endpoint(ESDTNFTBurn)]
+    fn esdt_nft_burn(&self, token_id: TokenIdentifier, nonce: u64, amount: BigUint) {
+        self.send().esdt_local_burn(&token_id, nonce, &amount);
+    }
 }
